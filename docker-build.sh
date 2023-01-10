8e59ca4c29e4248323877f448b3257500d0ca197
Aleksey Antropov <aleksey.antropov@corp.mail.ru>
2023-01-10 19:38:45 +0300

    style: the docker image tag is depends on branch name now

--- docker-build.sh
+++ docker-build.sh
@@ -2,4 +2,15 @@
 name='ssh-interview'
 tag=$(date '+%Y%m%d-%H%M%S')
 
+branch=$(git rev-parse --abbrev-ref HEAD)
+retval=$?
+if test $retval -ne 0; then
+    echo "The 'git' executable isnt found!"
+    exit 1
+fi
+
+if test "$branch" != "main"; then
+    tag="${branch}-${tag}"
+fi 
+
 docker build --tag ${name}:${tag} docker/
