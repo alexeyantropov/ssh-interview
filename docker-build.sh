d3c74e0514643aced6997faf1f568fe0be1cc540
Aleksey Antropov <aleksey.antropov@corp.mail.ru>
2023-01-13 13:28:04 +0300

    fix: dynamic base image tag for tasks images

--- docker-build.sh
+++ docker-build.sh
@@ -22,7 +22,8 @@ if test $(basename $0) = "docker-build.sh"; then
     docker build $build_args --tag ${name}:${tag} --tag ${name}:${latest_tag} docker/
     for task in tasks/*; do
         task_name=${name}_$(basename $task)
-        docker build $build_args --tag ${task_name}:${tag} --tag ${task_name}:${latest_tag} ${task}/
+        #cat ${task}/Dockerfile | sed s/__LATEST_TAG__/$latest_tag/g > ${task}/Dockerfile.tmp
+        docker build $build_args --tag ${task_name}:${tag} --tag ${task_name}:${latest_tag} --build-arg "_FROM_TAG=$latest_tag" ${task}/ 
     done
 elif test $(basename $0) = "docker-run.sh"; then
     run_opts='--rm -it --privileged --cap-add=SYS_PTRACE --publish 0.0.0.0:22222:22'
